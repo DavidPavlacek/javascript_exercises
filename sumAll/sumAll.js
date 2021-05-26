01bfae81a6e7739d3d973a8003fe7e2fb5f8b30c
DavidPavlacek <dav.pavlacek@gmail.com>
2021-05-26 18:06:17 +0200

    exercise completed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
-};
+    let finalSum = [];
 
+    if ( !Number.isInteger(a) || !Number.isInteger(b) || 
+         a < 0 || b < 0) { 
+         return "ERROR"
+    } else if (a > b) {
+         for ( a, b; b <= a; b++) {
+              finalSum.push(b); }
+    } else 
+         for ( a, b; a <= b; a++) {
+              finalSum.push(a); 
+    }    return finalSum.reduce((a, b) => a + b)
+}   
 module.exports = sumAll;
