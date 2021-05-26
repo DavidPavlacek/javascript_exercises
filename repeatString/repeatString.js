4633aea2727a82e341bff5cce0e01077af73e578
DavidPavlacek <dav.pavlacek@gmail.com>
2021-05-26 22:07:29 +0200

    excercise completed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+     
+    if (number < 0) {return "ERROR"};
+    if (text != isNaN()) { text = text.toString()};
+    if (number > 0) {
+         return text.repeat(number);
+    }else return "";
 };
-
 module.exports = repeatString;
