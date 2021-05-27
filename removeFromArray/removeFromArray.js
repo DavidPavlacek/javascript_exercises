b49f7a6d166c51e8150c3aa18e4b8accc3792e59
DavidPavlacek <dav.pavlacek@gmail.com>
2021-05-27 18:04:09 +0200

    exercise completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(...numbs) {
 
+    let argArray = numbs[0];
+    let filtered = argArray.filter(function(val){
+         return val = !numbs.includes(val)
+    }); return filtered
 };
-
 module.exports = removeFromArray;
