e43a916693dcb6022cbbdd42a94224669ec9155e
DavidPavlacek <dav.pavlacek@gmail.com>
2021-05-27 14:36:41 +0200

    exercise completed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    let strReverse = text.split("");
+    strReverse.reverse();
+    return strReverse.join("");
 };
 
 module.exports = reverseString;
