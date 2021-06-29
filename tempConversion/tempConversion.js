609cfaa485169aad7a15c60afcae32e0a8bd65ce
DavidPavlacek <dav.pavlacek@gmail.com>
2021-06-29 19:02:58 +0200

    exercise completed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
 
+  let celcius = (fahr - 32) * 0.556
+  let rounded = Math.round(celcius * 10) / 10
+  return rounded
 };
 
-const ctof = function() {
+const ctof = function(celc) {
 
+  let fahrenheit = celc * 1.8 + 32
+  let rounded = Math.round(fahrenheit * 10) / 10
+  return rounded
 };
 
 module.exports = {
